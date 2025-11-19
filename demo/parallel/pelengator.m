0d09f403f2505cafa1bd7be0e06509fb5c2c6158
justaneutral <justaneutral@gmail.com>
2025-11-19 05:07:27 -0500

    Added toggle switches. When toggle mask reset, if not updating mask, will write to mask file before reset and then toggle it off.

--- demo/parallel/pelengator.m
+++ demo/parallel/pelengator.m
@@ -2,7 +2,7 @@ close all
 %%%%==========parameters==========%%%%
 station_center_frequency    = [16000; 24000; 24000; 25000; 25200; 40750; 29350; 31900];
 station_bandwidth           = [   20;   300;   300;   400;   200;   300;   500;   400];
-station_max_peak_to_average = [    80;     40;    500;     80;     40;     30;    200;    200];
+station_max_peak_to_average = [    80;   40;   500;    80;    40;    30;   200;   200];
 station_min_peak_to_average = [    2;     1;    11;     4;     2;     2;     2;     2];
 station_mask_number         = [    1;     1;     2;     1;     1;     1;     1;     1];
 Fs = 500000;
@@ -15,6 +15,10 @@ panorama_mask_file_name = ['panorama_mask_' date '.mat'];
 panorama_floor_file_name  = ['panorama_floor_' date '.mat'];
 panorama_ceil_file_name  = ['panorama_ceil_' date '.mat'];
 squelsh_line_file_name = ['squelsh_line_' date '.mat'];
+modulation_mask_1_file_name = ['modulation_mask_1_' date '.mat'];
+modulation_mask_2_file_name = ['modulation_mask_2_' date '.mat'];
+modulation_mask_3_file_name = ['modulation_mask_3_' date '.mat'];
+preload_modulation_mask_file = [1,1,0];
 modulation_mask_gain = 0.07;
 station_relaxation_gain = 0.37;
 squelsh_relaxation_gain = 0.17;
@@ -26,6 +30,7 @@ Num_iterations = 1000000;
 num_keep = 100;
 trigtreshold = 15;
 panorama_gain = 4;
+
 %%%%==========averages============%%%%
 M = 2^(ceil(log2(N))-1)-1;
 colors = ['#ff0000'; '#00ff00'; '#0000ff'; '#000000'];
@@ -43,6 +48,18 @@ panorama_spawn = panorama_start:panorama_end;
 panorama_fscale = panorama_spawn*Fs/(2*(M+1));
 panorama_history = zeros(num_keep,N_panorama,3);
 modulation_mask = ones(3,N_panorama);
+if preload_modulation_mask_file(1) > 0
+        load(modulation_mask_1_file_name,"sfmm");
+        modulation_mask(1,:) = sfmm;
+end
+if preload_modulation_mask_file(2) > 0
+        load(modulation_mask_2_file_name,"sfmm");
+        modulation_mask(2,:) = sfmm;
+end
+if preload_modulation_mask_file(3) > 0
+        load(modulation_mask_3_file_name,"sfmm");
+        modulation_mask(3,:) = sfmm;
+end
 panorama_mask_n = zeros(1,N_panorama);
 %load('wrightstown_panorama_mask_true_energy.mat',"wrightstown_panorama_mask_true_energy");
 panorama_floor = 100000000000*ones(1,N_panorama); %wrightstown_panorama_mask_true_energy;
@@ -62,21 +79,41 @@ screenHeight = screenSize(4);
 topOffset = 50;
 figWidth = (screenWidth-2*topOffset)/5;
 figHeight = (screenHeight-2*topOffset)/4;
-figWidth1 = screenWidth-2*topOffset;
+figWidth1 = screenWidth/2-2*topOffset;
 figHeight1 = (screenHeight-2*topOffset)/2;
-fig1position = [screenWidth-5*figWidth-topOffset, screenHeight-figHeight-2*topOffset, figWidth, figHeight];
+fig1position = [screenWidth-5*figWidth-topOffset, screenHeight-figHeight-4*topOffset, figWidth, figHeight*1.5];
 fig3position = [screenWidth-4*figWidth, screenHeight-figHeight-2*topOffset, figWidth, figHeight];
 fig4position = [screenWidth-3*figWidth+1*topOffset, screenHeight-figHeight-2*topOffset, figWidth, figHeight];
 fig5position = [screenWidth-2*figWidth+2*topOffset, screenHeight-figHeight-2*topOffset, figWidth, figHeight];
 fig2position = [screenWidth-1*figWidth1-topOffset, screenHeight-figHeight-figHeight1-4*topOffset, figWidth1, figHeight1];
+controls_pos = [screenWidth-5*figWidth-topOffset, screenHeight-figHeight-figHeight1-4*topOffset, 0.9*figWidth, figHeight*1.8];
 %%%%========plotting angles=============%%%%
 fig_num = 4;
 subplot_x = ceil(sqrt(num_stations));
 subplot_y = subplot_x;
 
+%%%%==========controls============%%%%
+% Shared variable to store button pressed status
+% Create a UI figures
+userinterfacefigure = uifigure('Name', 'Button Press Monitor', 'Position', controls_pos);
+% Create a toggle switches
+toggleswitch_mask_update_3 = uiswitch(userinterfacefigure,'toggle','Items',{'Off mask 3 update','On mask 3 update'},'Position',[100 100 45 20],'Value','Off mask 3 update');
+toggleswitch_mask_reset_3 = uiswitch(userinterfacefigure,'toggle','Items',{'Off mask 3 reset','On mask 3 reset'},'Position',[200 100 45 20],'Value','Off mask 3 reset');
+toggleswitch_mask_update_2 = uiswitch(userinterfacefigure,'toggle','Items',{'Off mask 2 update','On mask 2 update'},'Position',[100 200 45 20],'Value','Off mask 2 update');
+toggleswitch_mask_reset_2 = uiswitch(userinterfacefigure,'toggle','Items',{'Off mask 2 reset','On mask 2 reset'},'Position',[200 200 45 20],'Value','Off mask 2 reset');
+toggleswitch_mask_update_1 = uiswitch(userinterfacefigure,'toggle','Items',{'Off mask 1 update','On mask 1 update'},'Position',[100 300 45 20],'Value','Off mask 1 update');
+toggleswitch_mask_reset_1 = uiswitch(userinterfacefigure,'toggle','Items',{'Off mask 1 reset','On mask 1 reset'},'Position',[200 300 45 20],'Value','Off mask 1 reset');
+toggleswitch_run_stop = uiswitch(userinterfacefigure,'toggle','Items',{'STOP','RUN'},'Position',[150 400 45 20],'Value','RUN');
+
 %%%%======loop========%%%%
 trigcounter = 0;
-for i=1:Num_iterations
+iteration_number = 0;
+while iteration_number < Num_iterations && isvalid(userinterfacefigure) ...
+         && isvalid(toggleswitch_run_stop) && toggleswitch_run_stop.Value(1)=='R' ...
+         && isvalid(toggleswitch_mask_update_1) && isvalid(toggleswitch_mask_reset_1) ...
+         && isvalid(toggleswitch_mask_update_2) && isvalid(toggleswitch_mask_reset_2) ...
+         && isvalid(toggleswitch_mask_update_3) && isvalid(toggleswitch_mask_reset_3)
+iteration_number = iteration_number+1;
 raw_buffer = read_file_helper(N)';
 signal_buffer = [ ...
     [raw_buffer(1:N), zeros(1,2*(M+1)-N)]; ...
@@ -135,13 +172,13 @@ r = r.*max_panorama;
 g = g.*max_panorama;
 b = b.*max_panorama;
 
-panorama_history(1+mod(i-1,num_keep),:,1) = r;
-panorama_history(1+mod(i-1,num_keep),:,2) = g;
-panorama_history(1+mod(i-1,num_keep),:,3) = b;
+panorama_history(1+mod(iteration_number-1,num_keep),:,1) = r;
+panorama_history(1+mod(iteration_number-1,num_keep),:,2) = g;
+panorama_history(1+mod(iteration_number-1,num_keep),:,3) = b;
 
 if squelsh_relaxation_gain > 1 
     panorama_mask_n = panorama_mask_n + max(panorama(1:3,:));
-    panorama_mask = panorama_mask_n/i;
+    panorama_mask = panorama_mask_n/iteration_number;
     panorama_ceil = max(panorama_ceil,max(panorama(1:3,:)));
 else
     panorama_mask_n = panorama_mask_n*(1-squelsh_relaxation_gain) + max(panorama(1:3,:))*squelsh_relaxation_gain;
@@ -212,17 +249,36 @@ for j=1:size(station_center_frequency,1)
         s_latch = squelsh_latch(:,s_spawn);
         s_current = sqrt(sum(s_sig(1:3,:).*conj(s_sig(1:3,:))));
         s_current = s_current/max(s_current);
-        s_modulation_mask = modulation_mask(s_mask,s_spawn-panorama_start+1);
-        %s_max = max(s_modulation_mask);
-        %s_current = s_current*s_max/max(s_current);
+        sfmm = modulation_mask(s_mask,:);
+        s_modulation_mask = sfmm(s_spawn-panorama_start+1);
+        s_max = max(s_modulation_mask);
+
+        %reset mask if swith on
+        if (toggleswitch_mask_reset_1.Value(2)=='n' && s_mask==1) || (toggleswitch_mask_reset_2.Value(2)=='n' && s_mask==2) ||(toggleswitch_mask_reset_3.Value(2)=='n' && s_mask==3)
+            if (toggleswitch_mask_update_1.Value(2)=='f' && s_mask==1)
+                save(modulation_mask_1_file_name,"sfmm");
+                toggleswitch_mask_reset_1.Value = 'Off mask 1 reset';
+            end
+            if (toggleswitch_mask_update_2.Value(2)=='f' && s_mask==2)
+                save(modulation_mask_2_file_name,"sfmm");
+                toggleswitch_mask_reset_2.Value = 'Off mask 2 reset';
+            end
+            if (toggleswitch_mask_update_3.Value(2)=='f' && s_mask==3)
+                save(modulation_mask_3_file_name,"sfmm");
+                toggleswitch_mask_reset_3.Value = 'Off mask 3 reset';
+            end
+
+            modulation_mask(s_mask,s_spawn-panorama_start+1) = ones(1,s_end-s_start+1);
+        end
+
         reference_angle = 0;
         subplot_p = j;
-        [directions(j,i),Pxys(j,i),Ans(j,i),IQs(j,:),ant_color] = measurement_correlational( ...
+        [directions(j,iteration_number),Pxys(j,iteration_number),Ans(j,iteration_number),IQs(j,:),ant_color] = measurement_correlational( ...
             station_max_peak_to_average(j), ...
             station_min_peak_to_average(j), ...
             IQs(j,:),station_relaxation_gain,s_sig,s_latch,s_modulation_mask,reference_angle,fig_num,fig4position,subplot_x,subplot_y,subplot_p);
-        if i>1 && Pxys(j,i) == 0
-            directions(j,i) = directions(j,i-1);
+        if iteration_number>1 && Pxys(j,iteration_number) == 0
+            directions(j,iteration_number) = directions(j,iteration_number-1);
             %directions(j,i) = 361;
         end
         set(0, 'DefaultFigurePosition', fig3position);
@@ -234,21 +290,31 @@ for j=1:size(station_center_frequency,1)
         plot(s_fscale,s_current,'color',ant_color);
         plot(s_fscale,s_modulation_mask,'color','yellow');
         axis([s_fscale(1) s_fscale(end) 0 s_max]);
-        if Ans(j,i) > 0
-            %only update if signal was present
-            modulation_mask(s_mask,s_spawn-panorama_start+1) = modulation_mask(s_mask,s_spawn-panorama_start+1)*(1-modulation_mask_gain) + s_current*modulation_mask_gain;
+        if Ans(j,iteration_number) > 0
+            %update if signal was present and ipdate swith on
+            if (toggleswitch_mask_update_1.Value(2)=='n' && s_mask==1) || (toggleswitch_mask_update_2.Value(2)=='n' && s_mask==2) ||(toggleswitch_mask_update_3.Value(2)=='n' && s_mask==3)  
+                modulation_mask(s_mask,s_spawn-panorama_start+1) = modulation_mask(s_mask,s_spawn-panorama_start+1)*(1-modulation_mask_gain) + s_current*modulation_mask_gain;
+            end
             set(0, 'DefaultFigurePosition', fig5position);
             figure(5);
-            if i > num_keep
-                plot(i-num_keep:i,directions(j,i-num_keep:i),'color',ant_color);
-                axis([i-num_keep i -90.5 90.5]);
+            if iteration_number > num_keep
+                plot(iteration_number-num_keep:iteration_number,directions(j,iteration_number-num_keep:iteration_number),'color',ant_color);
+                axis([iteration_number-num_keep iteration_number -90.5 90.5]);
             else
-                plot(1:i,directions(j,1:i),'color',ant_color);
+                plot(1:iteration_number,directions(j,1:iteration_number),'color',ant_color);
                 axis([1 num_keep -90.5 90.5]);
             end
             hold all;
         end
     end
 end
-
+drawnow; % Allow UI updates
+pause(0.1);
 end
+
+delete(userinterfacefigure)
+
+
+
+% Callback functions
+
